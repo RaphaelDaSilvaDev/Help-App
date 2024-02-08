ab7a1c5219e5ad1b82f5a71835aa802b92a4329d
RaphaelDaSilvaDev <raphael.h.silva@protonmail.com>
2024-02-08 18:54:38 -0300

    FEAT-2 #in-review add pagination to list answers

--- src/main/kotlin/com/raphaelsilva/help/app/controller/AnswerController.kt
+++ src/main/kotlin/com/raphaelsilva/help/app/controller/AnswerController.kt
@@ -2,21 +2,15 @@ package com.raphaelsilva.help.app.controller
 
 import com.raphaelsilva.help.app.dto.form.AnswerForm
 import com.raphaelsilva.help.app.dto.form.AnswerLikeForm
-import com.raphaelsilva.help.app.dto.view.AnswerLikeView
 import com.raphaelsilva.help.app.dto.view.AnswerWithChildrenCountView
 import com.raphaelsilva.help.app.dto.view.UserUnlikeView
 import com.raphaelsilva.help.app.service.AnswerService
-import org.springframework.http.HttpStatus
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.util.UriComponentsBuilder
 
 @RestController
@@ -24,35 +18,45 @@ import org.springframework.web.util.UriComponentsBuilder
 class AnswerController(private val answerService: AnswerService) {
 
     @PostMapping
-    fun createAnswer(@RequestBody answerForm: AnswerForm, uriBuilder: UriComponentsBuilder): ResponseEntity<AnswerWithChildrenCountView>{
+    fun createAnswer(
+        @RequestBody answerForm: AnswerForm,
+        uriBuilder: UriComponentsBuilder
+    ): ResponseEntity<AnswerWithChildrenCountView> {
         val answer = answerService.create(answerForm)
         val uri = uriBuilder.path("/api/answer/${answer.id}").build().toUri()
         return ResponseEntity.created(uri).body(answer)
     }
 
     @GetMapping("/{id}")
-    fun getById(@PathVariable id: Long): AnswerWithChildrenCountView{
+    fun getById(@PathVariable id: Long): AnswerWithChildrenCountView {
         return answerService.getById(id)
     }
 
     @GetMapping("/post/{postId}")
-    fun getByPostId(@PathVariable postId: Long): List<AnswerWithChildrenCountView>{
-        return answerService.getByPostId(postId)
+    fun getByPostId(
+        @PageableDefault(size = 10) pageable: Pageable,
+        @PathVariable postId: Long
+    ): Page<AnswerWithChildrenCountView> {
+        return answerService.getByPostId(postId, pageable)
     }
 
     @GetMapping("/father/{answerId}")
-    fun getAnswerByAnswerFather(@PathVariable answerId: Long): List<AnswerWithChildrenCountView>{
-        return answerService.getAnswerByAnswerFather(answerId)
+    fun getAnswerByAnswerFather(@PageableDefault(size = 10) pageable: Pageable ,@PathVariable answerId: Long):
+            Page<AnswerWithChildrenCountView> {
+        return answerService.getAnswerByAnswerFather(answerId, pageable)
     }
 
     @PutMapping("/like")
     fun addLike(@RequestBody answerLikeForm: AnswerLikeForm, uriBuilder: UriComponentsBuilder): Any {
         val like = answerService.likeController(answerLikeForm)
-        if(like != null){
+        if (like != null) {
             val uri = uriBuilder.path("/like").build().toUri()
             return ResponseEntity.created(uri).body(like)
         }
-        return ResponseEntity.ok().body(UserUnlikeView(message = "Unliked", userId = answerLikeForm.authorId,
-                                                       answerId = answerLikeForm.answerId))
+        return ResponseEntity.ok().body(
+            UserUnlikeView(
+                message = "Unliked", userId = answerLikeForm.authorId, answerId = answerLikeForm.answerId
+            )
+        )
     }
 }
--- src/main/kotlin/com/raphaelsilva/help/app/repository/AnswerRepository.kt
+++ src/main/kotlin/com/raphaelsilva/help/app/repository/AnswerRepository.kt
@@ -1,15 +1,21 @@
 package com.raphaelsilva.help.app.repository
 
 import com.raphaelsilva.help.app.model.Answer
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 
 interface AnswerRepository: JpaRepository<Answer, Long> {
-    fun findAllByAnswerId(id: Long): List<Answer>
+    fun findAllByAnswerId(id: Long, pageable: Pageable? = null): Page<Answer>
+
+    @Query(value = "SELECT * FROM Answer WHERE post_id = ?1 AND answer_id IS NULL", nativeQuery = true)
+    fun getAllByPostIdPageable(postId: Long, pageable: Pageable?): Page<Answer>
 
     @Query(value = "SELECT * FROM Answer WHERE post_id = ?1 AND answer_id IS NULL", nativeQuery = true)
     fun getAllByPostId(postId: Long): List<Answer>
+
 
     @Modifying
     @org.springframework.transaction.annotation.Transactional
